1725f228eb8e2d30a7170f25dac52c60fa7a9cdc
Martin <martin@yacc.io>
2016-02-02 19:42:55 +0000

    user_agent parsing

--- src/ua.rs
+++ src/ua.rs
@@ -38,12 +38,21 @@ impl UserAgentParser {
             let family = self.family.clone()
                 .and_then(|f| c.at(1).map(|a| f.replace("$1", a)))
                 .unwrap_or(c.at(1).unwrap().to_string());
+            let major = self.major.clone()
+                .and_then(|f| c.at(2).map(|a| f.replace("$2", a)))
+                .or(c.at(2).map(String::from));
+            let minor = self.minor.clone()
+                .and_then(|f| c.at(3).map(|a| f.replace("$3", a)))
+                .or(c.at(3).map(String::from));
+            let patch = self.patch.clone()
+                .and_then(|f| c.at(4).map(|a| f.replace("$4", a)))
+                .or(c.at(4).map(String::from));
 
             UserAgent {
                 family: family,
-                major: None,
-                minor: None,
-                patch: None,
+                major: major,
+                minor: minor,
+                patch: patch,
             }
         })
     }
